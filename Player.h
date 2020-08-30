51a406abdc37266b9b05584e11d108f8e76efac8
HGahir <43745312+HGahir@users.noreply.github.com>
2020-08-30 15:17:01 +0100

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+class Player
+{
+    public:
+        int PlayerHealth;
+        int PlayerStrength;
+        int PlayerEnergy;
+        int PlayerStamina;
+        int PlayerSkill;
+        int PlayerRespect;
+        bool JailCellVisited;
+        bool GymVisited;
+        std::string PlayerName = "";
+};
