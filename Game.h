4c488620d545fe703e6075ca16da5a4824fec45b
HGahir <43745312+HGahir@users.noreply.github.com>
2020-10-25 10:54:29 +0000

    Update Game.h

--- Game.h
+++ Game.h
@@ -17,58 +17,64 @@ class Game
             std::cout << "1.Start new game" << std::endl;
             std::cout << "2.Continue game" << std::endl;
             std::cout << "3.Edit Player Name" << std::endl;
-            std::cout << "4.Fight Debug" << std::endl;
+            std::cout << "4.Fight Debug" << std::endl; //Tests the fight function
             std::cout << "0.Quit Game" << std::endl;
             std::cout << "" << std::endl;
             std::cout << "-> ";
             std::cin >> PlayerMenuChoice;
-            if (PlayerMenuChoice == 0)
+            while (IsGameRunning)
             {
-                std::cout << "Quitting Game..." << std::endl;
-                IsGameRunning = false;
-                return;
-            }
-            else if (PlayerMenuChoice == 1)
-            {
-                if (player.PlayerName == "")
+                if (PlayerMenuChoice == 0)
                 {
-                    std::cout << "You must set a valid player name." << std::endl;
+                    std::cout << "Quitting Game..." << std::endl;
+                    IsGameRunning = false;
+                    exit(0);
                 }
-                else
+                else if (PlayerMenuChoice == 1)
                 {
-                    std::cout << "Starting new game..." << std::endl;
-                    GameStart();
+                    if (player.PlayerName == "")
+                    {
+                        std::cout << "You must set a valid player name." << std::endl;
+                        return;
+                    }
+                    else
+                    {
+                        std::cout << "Starting new game..." << std::endl;
+                        GameStart();
+                    }
                 }
-            }
-            if (PlayerMenuChoice == 2)
-            {
-                std::cout << "" << std::endl;
-            }
-            if (PlayerMenuChoice == 3)
-            {
-                std::cout << "Your current player name is: " << player.PlayerName << std::endl;
-                std::cout << "Enter Your new player name here: ";
-                std::cin >> player.PlayerName;
-                std::cout << "Your new player name is: " << player.PlayerName << std::endl;
-            }
-            if (PlayerMenuChoice == 4)
-            {
-                FightMode();
-            }
-            if (PlayerMenuChoice == 8)
-            {
-                IsGameRunning = false;
-                std::cout << "IsGameRunning: " << IsGameRunning << std::endl;
-            }
-            if (PlayerMenuChoice == 9)
-            {
-                std::cout << "IsGameRunning: " << IsGameRunning << std::endl;
+                if (PlayerMenuChoice == 2)
+                {
+                    std::cout << "" << std::endl;
+                }
+                if (PlayerMenuChoice == 3)
+                {
+                    std::cout << "Your current player name is: " << player.PlayerName << std::endl;
+                    std::cout << "Enter Your new player name here: ";
+                    std::cin >> player.PlayerName;
+                    std::cout << "Your new player name is: " << player.PlayerName << std::endl;
+                    return;
+                }
+                if (PlayerMenuChoice == 4)
+                {
+                    FightMode();
+                }
+                if (PlayerMenuChoice == 8)
+                {
+                    IsGameRunning = false;
+                    std::cout << "IsGameRunning: " << IsGameRunning << std::endl;
+                    return;
+                }
+                if (PlayerMenuChoice == 9)
+                {
+                    std::cout << "IsGameRunning: " << IsGameRunning << std::endl;
+                    return;
+                }
             }
         }
 
         void GameStart()
         {
-            int PlayerDifficulty;
             player.PlayerEnergy = 100;
             player.PlayerHealth = 100;
             player.PlayerSpeed = 10;
@@ -76,14 +82,32 @@ class Game
             player.PlayerSkill = 0;
             player.PlayerStamina = 10;
             player.PlayerStrength = 10;
+            std::string PlayerStartCommand;
 
             std::cout << "Welcome to the prison fight club " << player.PlayerName << std::endl;
             std::cout << "In this prison winning fights gets you benefits." << std::endl;
             std::cout << "Losing fights will decrease your respect among the other inmates resulting in poor treatment from inmates and prison guards." << std::endl;
             std::cout << "You must train yourself to increase your chances of winning fights, especially as the fights get harder." << std::endl;
             std::cout << "So " << player.PlayerName << "..." << std::endl;
-            std::cout << "Lets begin..." << std::endl;
+            std::cout << "Are you ready to begin?" << std::endl;
+            std::cout << "Type 'Yes' to begin" << std::endl;
+            std::cout << "-> ";
+            std::cin >> PlayerStartCommand;
 
+            if (PlayerStartCommand == "Yes")
+            {
+                GameJailCellIntro();
+            }
+            if (PlayerStartCommand == "0")
+            {
+                exit(0);
+            }
+            else
+            {
+                std::cout << "Invalid input. Try Again" << std::endl;
+                std::cout << "If you would like to quit type '0'." << std::endl;
+                GameStart();
+            }
         }
 
         void GameJailCellIntro()
@@ -91,25 +115,28 @@ class Game
             std::cout << "Welcome to your jail cell." << std::endl;
             std::cout << "Here you will find all your basic needs like food, water and rest." << std::endl;
             std::cout << "You can train once a day in the GYM to increase your strength, but be mindful of your ENERGY as going to the GYM will take some energy." << std::endl;
+            GameJailCell();
         }
 
         void GameJailCell()
         {
-            std::cout << "You are in your JAIL CELL." << std::endl;
-            std::cout << "Choose an option: " << std::endl;
-            std::cout << "1.Rest" << std::endl;
-            std::cout << "2.Go to the Gym" << std::endl;
-            std::cout << "3.Shop in the black market" << std::endl;
+
             int JailCellChoice;
             std::cin >> JailCellChoice;
             bool InJailCell = true;
             while (InJailCell)
             {
+            std::cout << "You are in your JAIL CELL." << std::endl;
+            std::cout << "Choose an option: " << std::endl;
+            std::cout << "1.Rest" << std::endl;
+            std::cout << "2.Go to the Gym" << std::endl;
+            std::cout << "3.Shop in the black market" << std::endl;
                 if (JailCellChoice == 1)
                 {
                     std::cout << "You curl up into to bed and rest for the night." << std::endl;
                     player.PlayerEnergy = 100;
                     player.PlayerHealth = 100;
+                    return;
                 }
 
                 if (JailCellChoice == 2)
@@ -124,20 +151,17 @@ class Game
                         {
                             player.PlayerStrength = player.PlayerStrength + 5;
                             player.GymVisited = true;
+                            return;
                         }
                         if (PlayerGymChoice == 2)
                         {
                             player.PlayerStamina = player.PlayerStamina + 5;
                             player.GymVisited = true;
+                            return;
                         }
                     }
                 }
             }
-        }
-
-        void DifficultySet(int SetDifficulty)
-        {
-            
         }
 
         void FightMode()
